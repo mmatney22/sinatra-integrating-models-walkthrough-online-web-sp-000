ebfb27a1eb5c746c8d4a5658c5faaf04e610e733
Morgan Matney <morgan.matney89@gmail.com>
2020-11-18 16:15:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class App < Sinatra::Base
     erb :index
   end
 
- post '/' do
-  @analyzed_text = TextAnalyzer.new(params[:user_text])
+  post '/' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
